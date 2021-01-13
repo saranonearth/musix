a04e5dc4587875e393e89c536e0bfa86bba82cd7
saranonearth <saran.suresh@outlook.com>
2021-01-14 01:47:16 +0530

    Naviagtion added

---
+++ app/screens/ProfileScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Screen from '../components/Screen';
+
+export default function ProfileScreen() {
+  return (
+    <Screen>
+      <Text>Profile</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
